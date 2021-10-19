f9b30f0f0126d12d1570b7ba58c9d5849c8b3372
RafaelCuberos <surfearr2@gmail.com>
2021-10-19 13:24:01 +0200

    Actualizando

---
+++ EntornoCliente/Primer Ejercicio AJAX/post.js
@@ -0,0 +1,68 @@
+/*vamos a suponer que tenemos 10 posts
+post=[
+    "primero",
+    "segundo",
+    "tercero",
+    "cuarto",
+    "quinto",
+    "sexto",
+    "septimo",
+    "ocatavo",
+    "noveno",
+    "decimo"
+]
+*/
+listado=document.getElementById("listado")
+
+//Vamos a recuperar los post que ya estan creados
+const postRecuperados=new XMLHttpRequest();
+postRecuperados.open('GET',"http://localhost:3000/posts");
+postRecuperados.send();
+
+postRecuperados.addEventListener('load',function(){
+if(postRecuperados.status==200){
+    let post=JSON.parse(postRecuperados.responseText);
+    sacarPosts(post)
+}
+else{
+    alert("Ha ocurrido un error al cargar los posts")
+}
+})
+
+function sacarPosts(post){
+barra=document.createElement("hr");
+listado.appendChild(barra);
+for (const i of post) {
+    nuevafila=document.createElement("div");
+    nuevafila.classList.add("seccion");
+    barra=document.createElement("hr");
+    info=`${i["title"]} - ${i["author"]}`,
+    texto=document.createTextNode(info);
+    
+    nuevafila.appendChild(texto)
+    listado.appendChild(nuevafila);
+
+    boton=crearVer();
+    listado.appendChild(boton);
+    boton=crearBorrar();
+    listado.appendChild(boton);
+
+    listado.appendChild(barra);
+   
+}
+}
+//Hay que pasarle los valores del enlace para poder crear los botones correctamente
+function crearBorrar(){
+    boton=document.createElement("input");
+    boton.value="borrar";
+    boton.setAttribute("type","button");
+    boton.classList.add("boton1");
+    return boton;
+}
+function crearVer(){
+    boton=document.createElement("input");
+    boton.value="Ver";
+    boton.setAttribute("type","button");
+    boton.classList.add("boton2");
+    return boton;
+}
---
+++ EntornoCliente/Primer Ejercicio AJAX/usuarios.js
@@ -0,0 +1,102 @@
+//Validacion del formulario
+nombre=document.getElementById("nombre");
+correo=document.getElementById("correo");
+dni=document.getElementById("dni");
+fecha=document.getElementById("fechanac");
+telefono=document.getElementById("telefono");
+boton=document.getElementById("boton")
+
+const formIsValid = {
+    nombre:false,
+    correo:false,
+    dni:false,
+    fecha:false,
+    telefono:false
+}
+
+boton.addEventListener('click', (e) =>{
+    validar();
+})
+
+nombre.addEventListener('change', (e) =>{
+    if (e.target.value.trim().length>1){
+        formIsValid.nombre=true;
+    }
+    else {
+        alert("El nombre introducido es incorrecto")
+        formIsValid.nombre=false;
+    }
+})
+
+correo.addEventListener('change', (e) =>{
+    regex= /.*@+[a-zA-Z]+\.(com|es)/gi;
+    if (regex.test(correo.value)){
+        formIsValid.correo=true;
+    }
+    else {
+        alert("El correo introducido es incorrecto")
+        formIsValid.correo=false;
+    }
+})
+
+dni.addEventListener('change', (e) =>{
+    regex= /[1-9][0-9]{7,8}/gi;
+    if (regex.test(dni.value) && dni.value.length==8){
+        formIsValid.dni=true;
+    }
+    else {
+        alert("El dni introducido es incorrecto");
+        formIsValid.dni=false;
+    }
+})
+
+fecha.addEventListener('change', (e) =>{
+    var partes = fecha.value.split("-");
+
+    if (partes[0]>1930 && partes[0]<2004){
+        formIsValid.fecha=true;}
+
+    else {
+        alert("La fecha introducida no es correcta");
+        formIsValid.fecha=false;
+    }
+    
+})
+
+telefono.addEventListener('change', (e) =>{
+    regex= /[6-7][0-9]{8}/gi;
+    if (regex.test(telefono.value)){
+        formIsValid.telefono=true;
+    }
+    else{
+        alert("El telefono introducido no es correcto");
+        formIsValid.telefono=false;
+    }
+})
+
+function validar(){
+    valores=Object.values(formIsValid);
+    valid=valores.findIndex(value => value == false);
+    if(valid == -1) {
+        alert("...ENVIANDO DATOS...");
+        enviarDatos();
+    } 
+    else alert('El formulario presenta errores, reviselo');
+}
+
+function enviarDatos(){
+    const nuevoUsuario={
+        name:document.getElementById("nombre").value,
+        mail:document.getElementById("correo").value,
+        document:document.getElementById("dni").value,
+        bdate:document.getElementById("fechanac").value,
+        phone:document.getElementById("telefono").value
+    }
+
+
+    const peticion= new XMLHttpRequest();
+    peticion.open('POST',"http://localhost:3000/users");
+    peticion.setRequestHeader('Content-type', 'application/json');
+    peticion.send(JSON.stringify(nuevoUsuario));
+
+}
